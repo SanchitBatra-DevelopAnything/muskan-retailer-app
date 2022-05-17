3b1701797d49295c48acf02d099a4eea9112f0de
Sanchit Batra <sanchitbatra@Sanchits-MacBook-Air.local>
2022-05-17 11:11:34 +0530

    finalizing item search

--- lib/items.dart
+++ lib/items.dart
@@ -12,24 +12,79 @@ class Items extends StatefulWidget {
 }
 
 class _ItemsState extends State<Items> {
+  var searchItemController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     final CategoriesProviderObject = Provider.of<CategoriesProvider>(context);
     final sub = CategoriesProviderObject.activeSubcategoryName;
     return Scaffold(
       backgroundColor: Colors.black54,
-      appBar: AppBar(title: Text(sub!)),
-      body: Column(children: [
-        Container(
-          color: Colors.red,
-          child: Center(
-              child: Card(
-            color: Colors.grey,
-            child:
-                TextField(decoration: InputDecoration(label: Text("Search"))),
-          )),
-        )
-      ]),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SizedBox(height: 35),
+          Flexible(
+            flex: 1,
+            child: Padding(
+              padding: EdgeInsets.only(left: 2, top: 25),
+              child: Row(
+                children: [
+                  Flexible(
+                      flex: 1,
+                      child: IconButton(
+                        icon: Icon(Icons.arrow_back_ios_new),
+                        color: Colors.white,
+                        iconSize: 20,
+                        onPressed: () {
+                          Navigator.of(context).pop();
+                        },
+                      )),
+                  Flexible(
+                    flex: 5,
+                    fit: FlexFit.tight,
+                    child: SizedBox(
+                      child: Card(
+                        clipBehavior: Clip.antiAliasWithSaveLayer,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10.0),
+                        ),
+                        child: TextField(
+                          style: TextStyle(
+                              fontSize: 18, fontWeight: FontWeight.bold),
+                          controller: searchItemController,
+                          decoration: InputDecoration(
+                            isDense: true,
+                            contentPadding: EdgeInsets.all(10),
+                            label: Padding(
+                              padding: EdgeInsets.only(left: 15),
+                              child: Text(
+                                "Search items here",
+                                style: TextStyle(fontSize: 15),
+                              ),
+                            ),
+                          ),
+                        ),
+                        elevation: 10,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                  Flexible(
+                      child: IconButton(
+                    iconSize: 30,
+                    icon: Icon(
+                      Icons.shopping_cart,
+                      color: Colors.white,
+                    ),
+                    onPressed: () {},
+                  ))
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
