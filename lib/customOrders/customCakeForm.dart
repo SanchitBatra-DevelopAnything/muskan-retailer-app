034a426c1225fb1d1244473d0343309cbba9ac8a
Sanchit Batra <sanchitbatra@Sanchits-MacBook-Air.local>
2022-06-28 09:21:12 +0530

    Asking before leaving the custom order page

--- lib/customOrders/customCakeForm.dart
+++ lib/customOrders/customCakeForm.dart
@@ -127,139 +127,163 @@ class _CustomCakeFormState extends State<CustomCakeForm> {
     final orderTypeMap =
         ModalRoute.of(context)!.settings.arguments as Map<String, String>;
     final orderType = orderTypeMap['orderType'];
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(orderType!,
-            style: TextStyle(
-              fontSize: 20,
-              color: Colors.white,
-              fontWeight: FontWeight.bold,
-            )),
-        backgroundColor: Colors.black,
-      ),
-      backgroundColor: Colors.white,
-      body: GestureDetector(
-        behavior: HitTestBehavior.opaque,
-        onTap: () {
-          FocusScope.of(context).unfocus();
-        },
-        child: Padding(
-          padding: EdgeInsets.all(20),
-          child: Center(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                Flexible(
-                    flex: 2,
-                    child: Container(
-                      height: 200,
-                      width: 200,
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.circular(20),
-                        child: CircleAvatar(
-                            backgroundColor: Color.fromRGBO(51, 51, 51, 1),
-                            backgroundImage: _pickedImage != null
-                                ? FileImage(_pickedImage!)
-                                : Image.asset("assets/default.png").image),
-                      ),
-                    )),
-                SizedBox(
-                  height: 15,
-                ),
-                Flexible(
-                  flex: 1,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      RaisedButton(
-                        color: Colors.red,
+    return WillPopScope(
+      onWillPop: () async {
+        bool willLeave = false;
+        // show the confirm dialog
+        await showDialog(
+            context: context,
+            builder: (_) => AlertDialog(
+                  title: const Text('Are you sure want to leave?'),
+                  actions: [
+                    ElevatedButton(
                         onPressed: () {
-                          _pickImage('Gallery');
+                          willLeave = true;
+                          Navigator.of(context).pop();
                         },
-                        child: Text(
-                          "Gallery Image",
-                          style: TextStyle(color: Colors.white, fontSize: 18),
+                        child: const Text('Yes')),
+                    TextButton(
+                        onPressed: () => Navigator.of(context).pop(),
+                        child: const Text('No'))
+                  ],
+                ));
+        return willLeave;
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(orderType!,
+              style: TextStyle(
+                fontSize: 20,
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+              )),
+          backgroundColor: Colors.black,
+        ),
+        backgroundColor: Colors.white,
+        body: GestureDetector(
+          behavior: HitTestBehavior.opaque,
+          onTap: () {
+            FocusScope.of(context).unfocus();
+          },
+          child: Padding(
+            padding: EdgeInsets.all(20),
+            child: Center(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Flexible(
+                      flex: 2,
+                      child: Container(
+                        height: 200,
+                        width: 200,
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(20),
+                          child: CircleAvatar(
+                              backgroundColor: Color.fromRGBO(51, 51, 51, 1),
+                              backgroundImage: _pickedImage != null
+                                  ? FileImage(_pickedImage!)
+                                  : Image.asset("assets/default.png").image),
                         ),
-                      ),
-                      RaisedButton(
-                        color: Colors.red,
-                        onPressed: () {
-                          _pickImage('Camera');
-                        },
-                        child: Text(
-                          "Camera Image",
-                          style: TextStyle(color: Colors.white, fontSize: 18),
-                        ),
-                      )
-                    ],
+                      )),
+                  SizedBox(
+                    height: 15,
                   ),
-                ),
-                SizedBox(
-                  height: 15,
-                ),
-                Flexible(
-                  flex: 3,
-                  child: Container(
-                    width: 300,
-                    child: TextFormField(
-                      style: TextStyle(fontSize: 18),
-                      minLines: 1,
-                      maxLines: null,
-                      controller: cakeDescriptionController,
-                      keyboardType: TextInputType.multiline,
-                      decoration: InputDecoration(
-                          contentPadding: const EdgeInsets.symmetric(
-                              vertical: 10.0, horizontal: 10.0),
-                          hintText: "Enter cake description here",
-                          hintStyle:
-                              TextStyle(color: Colors.black, fontSize: 18),
-                          border: OutlineInputBorder(
-                              borderRadius:
-                                  BorderRadius.all(Radius.circular(20)))),
-                    ),
-                  ),
-                ),
-                SizedBox(
-                  height: 15,
-                ),
-                Flexible(
-                  child: RaisedButton(
-                    color: Colors.red,
-                    child: Text(
-                      "Place Order",
-                      style: TextStyle(color: Colors.white, fontSize: 18),
-                    ),
-                    onPressed: () {
-                      placeCustomOrder(context, orderType);
-                    },
-                  ),
-                ),
-                Flexible(
+                  Flexible(
                     flex: 1,
-                    child: _isUploading
-                        ? Text(
-                            "Uploading Image.. Large images might take some time.",
-                            style: TextStyle(
-                                fontSize: 14, fontWeight: FontWeight.bold),
-                          )
-                        : _isFetchingUrl
-                            ? Text(
-                                "Fetching the url..",
-                                style: TextStyle(
-                                    fontSize: 14, fontWeight: FontWeight.bold),
-                              )
-                            : _isPlacingOrder
-                                ? Text(
-                                    "Placing your order....",
-                                    style: TextStyle(
-                                        fontSize: 14,
-                                        fontWeight: FontWeight.bold),
-                                  )
-                                : SizedBox(
-                                    height: 1,
-                                  ))
-              ],
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        RaisedButton(
+                          color: Colors.red,
+                          onPressed: () {
+                            _pickImage('Gallery');
+                          },
+                          child: Text(
+                            "Gallery Image",
+                            style: TextStyle(color: Colors.white, fontSize: 18),
+                          ),
+                        ),
+                        RaisedButton(
+                          color: Colors.red,
+                          onPressed: () {
+                            _pickImage('Camera');
+                          },
+                          child: Text(
+                            "Camera Image",
+                            style: TextStyle(color: Colors.white, fontSize: 18),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                  SizedBox(
+                    height: 15,
+                  ),
+                  Flexible(
+                    flex: 3,
+                    child: Container(
+                      width: 300,
+                      child: TextFormField(
+                        style: TextStyle(fontSize: 18),
+                        minLines: 1,
+                        maxLines: null,
+                        controller: cakeDescriptionController,
+                        keyboardType: TextInputType.multiline,
+                        decoration: InputDecoration(
+                            contentPadding: const EdgeInsets.symmetric(
+                                vertical: 10.0, horizontal: 10.0),
+                            hintText: "Enter cake description here",
+                            hintStyle:
+                                TextStyle(color: Colors.black, fontSize: 18),
+                            border: OutlineInputBorder(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(20)))),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 15,
+                  ),
+                  Flexible(
+                    child: RaisedButton(
+                      color: Colors.red,
+                      child: Text(
+                        "Place Order",
+                        style: TextStyle(color: Colors.white, fontSize: 18),
+                      ),
+                      onPressed: () {
+                        placeCustomOrder(context, orderType);
+                      },
+                    ),
+                  ),
+                  Flexible(
+                      flex: 1,
+                      child: _isUploading
+                          ? Text(
+                              "Uploading Image.. Large images might take some time.",
+                              style: TextStyle(
+                                  fontSize: 14, fontWeight: FontWeight.bold),
+                            )
+                          : _isFetchingUrl
+                              ? Text(
+                                  "Fetching the url..",
+                                  style: TextStyle(
+                                      fontSize: 14,
+                                      fontWeight: FontWeight.bold),
+                                )
+                              : _isPlacingOrder
+                                  ? Text(
+                                      "Placing your order....",
+                                      style: TextStyle(
+                                          fontSize: 14,
+                                          fontWeight: FontWeight.bold),
+                                    )
+                                  : SizedBox(
+                                      height: 1,
+                                    ))
+                ],
+              ),
             ),
           ),
         ),
