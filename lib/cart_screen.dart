3115d128d993b8a09478c3d7c7d248f458efc833
Sanchit Batra <sanchitbatra@Sanchits-MacBook-Air.local>
2022-09-04 14:22:57 +0530

    Cart save automatic

--- lib/cart_screen.dart
+++ lib/cart_screen.dart
@@ -141,158 +141,175 @@ class _CartScreenState extends State<CartScreen> {
     final authProviderObject =
         Provider.of<AuthProvider>(context, listen: false);
 
-    return Scaffold(
-      backgroundColor: Colors.black54,
-      body: _isPlacingOrder
-          ? Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Center(
-                  child: CircularProgressIndicator(
-                    color: Colors.red,
-                    strokeWidth: 5,
-                  ),
-                ),
-                Text(
-                  "Placing Your Order...",
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold),
-                )
-              ],
-            )
-          : Column(
-              children: [
-                SizedBox(height: 35),
-                Flexible(
-                  flex: 2,
-                  child: Padding(
-                    padding: EdgeInsets.only(left: 2, top: 25),
-                    child: Row(
-                      children: [
-                        Flexible(
-                            flex: 1,
-                            child: IconButton(
-                              icon: Icon(Icons.arrow_back_ios_new),
-                              color: Colors.white,
-                              iconSize: 20,
-                              onPressed: () {
-                                if (cartItemsList.length == 0) {
-                                  Navigator.of(context).pop();
-                                } else {
-                                  showSaveCartDialog(
-                                      context,
-                                      cartProviderObject,
-                                      authProviderObject.loggedInRetailer,
-                                      authProviderObject.loggedInShop);
-                                }
-                              },
-                            )),
-                        Flexible(
-                          flex: 5,
-                          fit: FlexFit.tight,
-                          child: Text(
-                            "YOUR CART",
-                            style: TextStyle(
-                                fontSize: 25,
-                                color: Colors.white,
-                                fontWeight: FontWeight.bold,
-                                fontStyle: FontStyle.normal),
-                          ),
-                        ),
-                        Spacer(),
-                        Flexible(
-                          flex: 4,
-                          child: Container(
-                            margin: EdgeInsets.only(right: 15),
-                            width: 300,
-                            height: 50,
-                            child: totalItems != 0
-                                ? RaisedButton(
-                                    onPressed: () {
-                                      showOrderDialog(
-                                          context, cartProviderObject);
-                                    },
-                                    color: Color.fromRGBO(51, 51, 51, 1),
-                                    child: AnimatedTextKit(
-                                        onTap: () {
-                                          cartProviderObject
-                                              .deleteCartOnDB(
-                                                  authProviderObject
-                                                      .loggedInRetailer,
-                                                  authProviderObject
-                                                      .loggedInShop)
-                                              .then((_) => {
-                                                    showOrderDialog(context,
-                                                        cartProviderObject)
-                                                  });
-                                        },
-                                        repeatForever: true,
-                                        isRepeatingAnimation: true,
-                                        animatedTexts: [
-                                          TyperAnimatedText("Click here",
-                                              textStyle: TextStyle(
-                                                  fontSize: 18,
-                                                  fontWeight: FontWeight.bold,
-                                                  color: Colors.white)),
-                                          TyperAnimatedText("Checkout",
-                                              textStyle: TextStyle(
-                                                  fontSize: 18,
-                                                  fontWeight: FontWeight.bold,
-                                                  color: Colors.white))
-                                        ]))
-                                : SizedBox(
-                                    height: 0,
-                                  ),
-                          ),
-                        )
-                      ],
+    return WillPopScope(
+      onWillPop: () async {
+        setState(() {
+          _isSavingCart = true;
+        });
+        await cartProviderObject.saveCart(authProviderObject.loggedInRetailer,
+            authProviderObject.loggedInShop);
+        setState(() {
+          _isSavingCart = false;
+        });
+        return true;
+      },
+      child: Scaffold(
+        backgroundColor: Colors.black54,
+        body: _isPlacingOrder
+            ? Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Center(
+                    child: CircularProgressIndicator(
+                      color: Colors.red,
+                      strokeWidth: 5,
                     ),
                   ),
-                ),
-                Divider(
-                  color: Colors.white,
-                  thickness: 5,
-                ),
-                _isSavingCart
-                    ? Center(
-                        child: CircularProgressIndicator(color: Colors.red),
-                      )
-                    : totalItems != 0
-                        ? Flexible(
-                            flex: 10,
-                            child: ListView.builder(
-                                itemExtent: 100,
-                                itemCount: cartItemsList.length,
-                                itemBuilder: ((context, index) =>
-                                    CartProductCard(
-                                        cartItem: cartItemsList[index]))))
-                        : Center(
-                            child: Text(
-                            "Cart is Empty.",
-                            style: TextStyle(
-                                fontSize: 16,
-                                color: Colors.white,
-                                fontWeight: FontWeight.bold),
-                          )),
-                Flexible(
-                    child: Divider(
-                  thickness: 5,
-                  color: Colors.white,
-                )),
-                Flexible(
-                  child: Text(
-                    'TOTAL : Rs.${totalOrderPrice}',
+                  Text(
+                    "Placing Your Order...",
                     style: TextStyle(
                         color: Colors.white,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 20),
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold),
+                  )
+                ],
+              )
+            : Column(
+                children: [
+                  SizedBox(height: 35),
+                  Flexible(
+                    flex: 2,
+                    child: Padding(
+                      padding: EdgeInsets.only(left: 2, top: 25),
+                      child: Row(
+                        children: [
+                          Flexible(
+                              flex: 1,
+                              child: IconButton(
+                                icon: Icon(Icons.arrow_back_ios_new),
+                                color: Colors.white,
+                                iconSize: 20,
+                                onPressed: () async {
+                                  if (cartItemsList.length == 0) {
+                                    Navigator.of(context).pop();
+                                  }
+                                  setState(() {
+                                    _isSavingCart = true;
+                                  });
+                                  await cartProviderObject.saveCart(
+                                      authProviderObject.loggedInRetailer,
+                                      authProviderObject.loggedInShop);
+                                  setState(() {
+                                    _isSavingCart = false;
+                                  });
+                                  Navigator.of(context).pop();
+                                },
+                              )),
+                          Flexible(
+                            flex: 5,
+                            fit: FlexFit.tight,
+                            child: Text(
+                              "YOUR CART",
+                              style: TextStyle(
+                                  fontSize: 25,
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                  fontStyle: FontStyle.normal),
+                            ),
+                          ),
+                          Spacer(),
+                          Flexible(
+                            flex: 4,
+                            child: Container(
+                              margin: EdgeInsets.only(right: 15),
+                              width: 300,
+                              height: 50,
+                              child: totalItems != 0
+                                  ? RaisedButton(
+                                      onPressed: () {
+                                        showOrderDialog(
+                                            context, cartProviderObject);
+                                      },
+                                      color: Color.fromRGBO(51, 51, 51, 1),
+                                      child: AnimatedTextKit(
+                                          onTap: () {
+                                            cartProviderObject
+                                                .deleteCartOnDB(
+                                                    authProviderObject
+                                                        .loggedInRetailer,
+                                                    authProviderObject
+                                                        .loggedInShop)
+                                                .then((_) => {
+                                                      showOrderDialog(context,
+                                                          cartProviderObject)
+                                                    });
+                                          },
+                                          repeatForever: true,
+                                          isRepeatingAnimation: true,
+                                          animatedTexts: [
+                                            TyperAnimatedText("Click here",
+                                                textStyle: TextStyle(
+                                                    fontSize: 18,
+                                                    fontWeight: FontWeight.bold,
+                                                    color: Colors.white)),
+                                            TyperAnimatedText("Checkout",
+                                                textStyle: TextStyle(
+                                                    fontSize: 18,
+                                                    fontWeight: FontWeight.bold,
+                                                    color: Colors.white))
+                                          ]))
+                                  : SizedBox(
+                                      height: 0,
+                                    ),
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
                   ),
-                )
-              ],
-            ),
+                  Divider(
+                    color: Colors.white,
+                    thickness: 5,
+                  ),
+                  _isSavingCart
+                      ? Center(
+                          child: CircularProgressIndicator(color: Colors.red),
+                        )
+                      : totalItems != 0
+                          ? Flexible(
+                              flex: 10,
+                              child: ListView.builder(
+                                  itemExtent: 100,
+                                  itemCount: cartItemsList.length,
+                                  itemBuilder: ((context, index) =>
+                                      CartProductCard(
+                                          cartItem: cartItemsList[index]))))
+                          : Center(
+                              child: Text(
+                              "Cart is Empty.",
+                              style: TextStyle(
+                                  fontSize: 16,
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold),
+                            )),
+                  Flexible(
+                      child: Divider(
+                    thickness: 5,
+                    color: Colors.white,
+                  )),
+                  Flexible(
+                    child: Text(
+                      'TOTAL : Rs.${totalOrderPrice}',
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20),
+                    ),
+                  )
+                ],
+              ),
+      ),
     );
   }
 }
