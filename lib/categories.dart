9e80a19ee1ee8a981ece4f74d79fa7db9d6545ad
Sanchit Batra <sanchitbatra@Sanchits-MacBook-Air.local>
2023-04-04 01:21:36 +0530

    Gone version release

--- lib/categories.dart
+++ lib/categories.dart
@@ -41,8 +41,6 @@ class _CategoriesState extends State<Categories> {
     // 1. This method call when app in terminated state and you get a notification
     // when you click on notification app open from terminated state and you can get notification data in this method
 
-    FirebaseMessaging.instance.subscribeToTopic("newArrivals");
-
     FirebaseMessaging.instance.getInitialMessage().then(
       (message) {
         print("FirebaseMessaging.instance.getInitialMessage");
